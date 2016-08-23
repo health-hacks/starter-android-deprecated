efa103c762751e4a1eb57d25733a35968b83dd15
Tian Hao <thao@us.ibm.com>
2016-08-23 14:32:24 -0400

    initial commit

---
+++ app/src/main/java/com/ibm/us/googlefittoolset/MainActivity.java
@@ -0,0 +1,381 @@
+package com.ibm.us.googlefittoolset;
+
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.Scopes;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Scope;
+
+import com.google.android.gms.fitness.Fitness;
+import com.google.android.gms.fitness.FitnessStatusCodes;
+
+import com.google.android.gms.fitness.data.Bucket;
+import com.google.android.gms.fitness.data.DataPoint;
+import com.google.android.gms.fitness.data.DataSet;
+import com.google.android.gms.fitness.data.Field;
+import com.google.android.gms.fitness.data.DataType;
+import com.google.android.gms.fitness.data.Subscription;
+
+import com.google.android.gms.fitness.result.ListSubscriptionsResult;
+import com.google.android.gms.fitness.result.DataReadResult;
+
+import com.google.android.gms.fitness.request.DataReadRequest;
+
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static java.text.DateFormat.getDateInstance;
+import static java.text.DateFormat.getTimeInstance;
+
+
+
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    public static final String TAG = "GoogleFitToolSet";
+
+    private GoogleApiClient mClient = null;
+
+    TextView tv_info;
+    TextView tv_list_info;
+
+    // List View for displaying Google Fit data
+    ListView listView;
+    ArrayAdapter<String> listAdapter;
+    int defaultResourceID = android.R.layout.simple_list_item_1;
+
+    String[] strListView = {"Weight", "Calories", "Activity Samples"};
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv_info = (TextView) findViewById(R.id.tv_main_info);
+        tv_info.setText("> Welcome to Google Toolset! \n");
+        tv_info.setMovementMethod(new ScrollingMovementMethod());
+
+        tv_list_info = (TextView) findViewById(R.id.tv_list_info);
+        tv_list_info.setText("Subscribed Data Types:");
+
+        listAdapter = new ArrayAdapter<String>(this, defaultResourceID, strListView);
+        listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(listAdapter);
+
+        listView.setOnItemClickListener(this);
+
+        buildFitnessClient();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+
+
+
+    }
+
+
+    // Create a message handling object as an anonymous class.
+    public void onItemClick(AdapterView parent, View v, int position, long id) {
+        // Do something in response to the click
+        //listAdapter.add("ADD");
+        //listAdapter.notifyDataSetChanged();
+    }
+
+
+    /**
+     * Build a {@link GoogleApiClient} that will authenticate the user and allow the application
+     * to connect to Fitness APIs. The scopes included should match the scopes your app needs
+     * (see documentation for details). Authentication will occasionally fail intentionally,
+     * and in those cases, there will be a known resolution, which the OnConnectionFailedListener()
+     * can address. Examples of this include the user never having signed in before, or
+     * having multiple accounts on the device and needing to specify which account to use, etc.
+     * More details on requesting scopes:
+     * https://developers.google.com/fit/android/authorization#scopes
+     */
+    private void buildFitnessClient() {
+        // Create the Google API Client
+        mClient = new GoogleApiClient.Builder(this)
+                // ADD API and SCOPEs HERE
+                .addApi(Fitness.RECORDING_API)
+                .addApi(Fitness.HISTORY_API)
+                .addScope(new Scope(Scopes.FITNESS_ACTIVITY_READ_WRITE))
+                .addScope(new Scope(Scopes.FITNESS_BODY_READ_WRITE))
+                .addScope(new Scope(Scopes.FITNESS_NUTRITION_READ_WRITE))
+                .addScope(new Scope(Scopes.FITNESS_LOCATION_READ_WRITE))
+                .addConnectionCallbacks(
+                        new GoogleApiClient.ConnectionCallbacks() {
+                            @Override
+                            public void onConnected(Bundle bundle) {
+                                Log.i(TAG, "Connected!!!");
+                                tv_info.append("> App connected to Google Fit \n");
+
+                                // Now you can make calls to the Fitness APIs.  What to do?
+                                // Look at some data!!
+
+                                // Subscribe to some data sources!
+                                subscribe(DataType.TYPE_WEIGHT);
+                                subscribe(DataType.TYPE_CALORIES_EXPENDED);
+                                subscribe(DataType.TYPE_ACTIVITY_SAMPLE);
+
+                                // Show the list of subscriptions
+                                dumpSubscriptionsList();
+
+                                //DataReadRequest readRequest = queryFitnessData();
+                                DataReadRequest readRequest = queryFitnessData(DataType.TYPE_WEIGHT);
+
+                                // [START read_dataset]
+                                // Invoke the History API to fetch the data with the query and await the result of
+                                // the read request.
+                                Fitness.HistoryApi.readData(mClient, readRequest)
+                                        .setResultCallback(new ResultCallback<DataReadResult>() {
+                                            @Override
+                                            public void onResult(DataReadResult dataReadResult) {
+                                                // For the sake of the sample, we'll print the data so we can see what we just added.
+                                                // In general, logging fitness information should be avoided for privacy reasons.
+
+                                                printData(dataReadResult);
+                                            }
+                                        });
+                                // [END read_dataset]
+
+                            }
+
+                            @Override
+                            public void onConnectionSuspended(int i) {
+                                // If your connection to the sensor gets lost at some point,
+                                // you'll be able to determine the reason and react to it here.
+                                if (i == GoogleApiClient.ConnectionCallbacks.CAUSE_NETWORK_LOST) {
+                                    Log.i(TAG, "Connection lost.  Cause: Network Lost.");
+                                    tv_info.append("> Connection lost.  Cause: Network Lost. \n");
+                                } else if (i == GoogleApiClient.ConnectionCallbacks.CAUSE_SERVICE_DISCONNECTED) {
+                                    Log.i(TAG, "Connection lost.  Reason: Service Disconnected");
+                                    tv_info.append("> Connection lost.  Reason: Service Disconnected \n");
+                                }
+                            }
+                        }
+                )
+                .enableAutoManage(this, 0, new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(ConnectionResult result) {
+                        Log.i(TAG, "Google Play services connection failed. Cause: " +
+                                result.toString());
+                        tv_info.append("> Google Play services connection failed. Cause: " +
+                                result.toString() + "\n");
+                    }
+                })
+                .build();
+    }
+
+
+    /**
+     * Subscribe to an available {@link DataType}. Subscriptions can exist across application
+     * instances (so data is recorded even after the application closes down).  When creating
+     * a new subscription, it may already exist from a previous invocation of this app.  If
+     * the subscription already exists, the method is a no-op.  However, you can check this with
+     * a special success code.
+     * More details about subscribable data types:
+     * https://developers.google.com/android/reference/com/google/android/gms/fitness/data/DataType
+     */
+    public void subscribe(DataType dataType) {
+        // To create a subscription, invoke the Recording API. As soon as the subscription is
+        // active, fitness data will start recording.
+        // [START subscribe_to_datatype]
+        Fitness.RecordingApi.subscribe(mClient, dataType)
+                .setResultCallback(new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(Status status) {
+                        if (status.isSuccess()) {
+                            if (status.getStatusCode()
+                                    == FitnessStatusCodes.SUCCESS_ALREADY_SUBSCRIBED) {
+                                Log.i(TAG, "Existing subscription for activity detected.");
+                            } else {
+                                Log.i(TAG, "Successfully subscribed!");
+                            }
+                        } else {
+                            Log.i(TAG, "There was a problem subscribing.");
+                            Log.i(TAG, status.toString() + " / " + status.getStatusMessage());
+                        }
+                    }
+                });
+        // [END subscribe_to_datatype]
+    }
+
+
+    /**
+     * Fetch a list of all active subscriptions and log it. Since the logger for this sample
+     * also prints to the screen, we can see what is happening in this way.
+     */
+    private void dumpSubscriptionsList() {
+        // [START list_current_subscriptions]
+        Fitness.RecordingApi.listSubscriptions(mClient)
+                // Create the callback to retrieve the list of subscriptions asynchronously.
+                .setResultCallback(new ResultCallback<ListSubscriptionsResult>() {
+                    @Override
+                    public void onResult(ListSubscriptionsResult listSubscriptionsResult) {
+                        for (Subscription sc : listSubscriptionsResult.getSubscriptions()) {
+                            DataType dt = sc.getDataType();
+                            Log.i(TAG, "Active subscription for data type: " + dt.getName());
+                            tv_info.append("> Active subscription for data type: " + dt.getName() + "\n");
+                        }
+                    }
+                });
+        // [END list_current_subscriptions]
+    }
+
+
+    /**
+     * Cancel the ACTIVITY_SAMPLE subscription by calling unsubscribe on that {@link DataType}.
+     */
+    private void cancelSubscription() {
+        final String dataTypeStr = DataType.TYPE_ACTIVITY_SAMPLE.toString();
+        Log.i(TAG, "Unsubscribing from data type: " + dataTypeStr);
+
+        // Invoke the Recording API to unsubscribe from the data type and specify a callback that
+        // will check the result.
+        // [START unsubscribe_from_datatype]
+        Fitness.RecordingApi.unsubscribe(mClient, DataType.TYPE_ACTIVITY_SAMPLE)
+                .setResultCallback(new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(Status status) {
+                        if (status.isSuccess()) {
+                            Log.i(TAG, "Successfully unsubscribed for data type: " + dataTypeStr);
+                        } else {
+                            // Subscription not removed
+                            Log.i(TAG, "Failed to unsubscribe for data type: " + dataTypeStr);
+                        }
+                    }
+                });
+        // [END unsubscribe_from_datatype]
+    }
+
+
+    /**
+     * Return a {@link DataReadRequest} for all step count changes in the past week.
+     */
+    public static DataReadRequest queryFitnessData() {
+        // [START build_read_data_request]
+        // Setting a start and end date using a range of 1 week before this moment.
+        Calendar cal = Calendar.getInstance();
+        Date now = new Date();
+        cal.setTime(now);
+        long endTime = cal.getTimeInMillis();
+        cal.add(Calendar.WEEK_OF_YEAR, -1);
+        long startTime = cal.getTimeInMillis();
+
+
+        DataReadRequest readRequest = new DataReadRequest.Builder()
+                // The data request can specify multiple data types to return, effectively
+                // combining multiple data queries into one call.
+                // In this example, it's very unlikely that the request is for several hundred
+                // datapoints each consisting of a few steps and a timestamp.  The more likely
+                // scenario is wanting to see how many steps were walked per day, for 7 days.
+                .aggregate(DataType.TYPE_STEP_COUNT_DELTA, DataType.AGGREGATE_STEP_COUNT_DELTA)
+                // Analogous to a "Group By" in SQL, defines how data should be aggregated.
+                // bucketByTime allows for a time span, whereas bucketBySession would allow
+                // bucketing by "sessions", which would need to be defined in code.
+                .bucketByTime(1, TimeUnit.DAYS)
+                .setTimeRange(startTime, endTime, TimeUnit.MILLISECONDS)
+                .build();
+        // [END build_read_data_request]
+
+        return readRequest;
+    }
+
+
+    /**
+     * Return a {@link DataReadRequest} for all step count changes in the past week.
+     */
+    public static DataReadRequest queryFitnessData(DataType dataType) {
+        // [START build_read_data_request]
+        // Setting a start and end date using a range of 1 week before this moment.
+        Calendar cal = Calendar.getInstance();
+        Date now = new Date();
+        cal.setTime(now);
+        long endTime = cal.getTimeInMillis();
+        cal.add(Calendar.WEEK_OF_YEAR, -4);
+        long startTime = cal.getTimeInMillis();
+
+
+        DataReadRequest readRequest = new DataReadRequest.Builder()
+                .read(dataType)
+                .setTimeRange(startTime, endTime, TimeUnit.MILLISECONDS)
+                .build();
+        // [END build_read_data_request]
+
+        return readRequest;
+    }
+
+
+    /**
+     * Log a record of the query result. It's possible to get more constrained data sets by
+     * specifying a data source or data type, but for demonstrative purposes here's how one would
+     * dump all the data. In this sample, logging also prints to the device screen, so we can see
+     * what the query returns, but your app should not log fitness information as a privacy
+     * consideration. A better option would be to dump the data you receive to a local data
+     * directory to avoid exposing it to other applications.
+     */
+    public static void printData(DataReadResult dataReadResult) {
+        // [START parse_read_data_result]
+        // If the DataReadRequest object specified aggregated data, dataReadResult will be returned
+        // as buckets containing DataSets, instead of just DataSets.
+        if (dataReadResult.getBuckets().size() > 0) {
+            Log.i(TAG, "Number of returned buckets of DataSets is: "
+                    + dataReadResult.getBuckets().size());
+            for (Bucket bucket : dataReadResult.getBuckets()) {
+                List<DataSet> dataSets = bucket.getDataSets();
+                for (DataSet dataSet : dataSets) {
+                    dumpDataSet(dataSet);
+                }
+            }
+        } else if (dataReadResult.getDataSets().size() > 0) {
+            Log.i(TAG, "Number of returned DataSets is: "
+                    + dataReadResult.getDataSets().size());
+            for (DataSet dataSet : dataReadResult.getDataSets()) {
+                dumpDataSet(dataSet);
+            }
+        }
+        // [END parse_read_data_result]
+    }
+
+    // [START parse_dataset]
+    private static void dumpDataSet(DataSet dataSet) {
+        Log.i(TAG, "Data returned for Data type: " + dataSet.getDataType().getName());
+        DateFormat dateFormat = getTimeInstance();
+
+        for (DataPoint dp : dataSet.getDataPoints()) {
+            Log.i(TAG, "Data point:");
+            Log.i(TAG, "\tType: " + dp.getDataType().getName());
+            Log.i(TAG, "\tStart: " + dateFormat.format(dp.getStartTime(TimeUnit.MILLISECONDS)));
+            Log.i(TAG, "\tEnd: " + dateFormat.format(dp.getEndTime(TimeUnit.MILLISECONDS)));
+            for (Field field : dp.getDataType().getFields()) {
+                Log.i(TAG, "\tField: " + field.getName() +
+                        " Value: " + dp.getValue(field));
+            }
+        }
+    }
+
+}
